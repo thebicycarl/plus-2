076d605da1acac59fb92ff20a2cc35e971f6e1ed
CarlHockey <thebicycarl@gmail.com>
2024-02-22 09:55:58 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const day = document.getElementById('day');
+const theDate = document.getElementById('date');
+const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const currentDate = new Date();
+
+let dateNumber = currentDate.getDate();
+let dateDay = days[currentDate.getDay() - 1];
+let dateMonth = months[currentDate.getMonth() - 1];
+
+day.innerText = dateDay;
+theDate.innerText = `${dateNumber} ${dateMonth}`;
